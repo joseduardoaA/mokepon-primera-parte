cbc70378bfa73917b9773f44ea1d032a8b53aaaf
José Eduardo Antúnez Aguilar <jose.cfeteit@gmail.com>
2024-01-30 19:41:35 -0600

    Colisione,mapa y enemigos

--- mokepon.js
+++ mokepon.js
@@ -40,30 +40,47 @@ let vidasJugador = 3
 let vidasEnemigo = 3
 let lienzo = mapa.getContext("2d")
 let intervalo 
+let mapaBackground = new Image()
+mapaBackground.src = "mokemap.png"
+let mascotaJugadorObjeto
 
 class Mokepon {
-    constructor(nombre, foto, vida) {
+    constructor(nombre, foto, vida, fotoMapa,x = 10, y = 10) {
         this.nombre = nombre
         this.foto = foto
         this.vida = vida
         this.ataques = []
-        this.x = 20
-        this.y = 30
-        this.ancho = 80
-        this.alto = 80
+        this.x = x
+        this.y = y
+        this.ancho = 40
+        this.alto = 40
         this.mapaFoto = new Image()
-        this.mapaFoto.src = foto
+        this.mapaFoto.src = fotoMapa
         this.velocidadX = 0
         this.velocidadY = 0
 
+    }
+    pintarMokepon() {
+        lienzo.drawImage(
+            this.mapaFoto, 
+            this.x,
+            this.y,
+            this.ancho,
+            this.alto
+            )
     }
     
 }
 
 // Estos son Objetos-Instancia porque se construyen a partir de una clase, solo les colocamos las propiedades, la clase Mokepon
-let hipodoge = new Mokepon("hipodoge","mokepons_mokepon_hipodoge_attack.png", 3 )
-let capipepo = new Mokepon("capipepo","mokepons_mokepon_capipepo_attack.png", 3 )
-let ratigueya = new Mokepon("ratigueya","mokepons_mokepon_ratigueya_attack.png", 3 )
+let hipodoge = new Mokepon("hipodoge","mokepons_mokepon_hipodoge_attack.png", 5, "hipodoge.png")
+let capipepo = new Mokepon("capipepo","mokepons_mokepon_capipepo_attack.png", 5, "capipepo.png" )
+let ratigueya = new Mokepon("ratigueya","mokepons_mokepon_ratigueya_attack.png", 5, "ratigueya.png" )
+
+let hipodogeEnemigo = new Mokepon("hipodoge","mokepons_mokepon_hipodoge_attack.png", 5, "hipodoge.png", 80, 120)
+let capipepoEnemigo = new Mokepon("capipepo","mokepons_mokepon_capipepo_attack.png", 5, "capipepo.png", 150,95 )
+let ratigueyaEnemigo = new Mokepon("ratigueya","mokepons_mokepon_ratigueya_attack.png", 5, "ratigueya.png",200,190 )
+
 
 /* Esto es un Objeto Literal poruqe se crea de 0 y solo van a guardar informacion
  Creo un Objeto, le coloco nombre y su ID que es la misma que en el HTML  */
@@ -115,11 +132,7 @@ function iniciarJuego() {
 }
 function seleccionarMacotaJugador () {
     sectionSeleccionarMascota.style.display = "none"
-    //sectionSeleccionarAtaque.style.display = "flex"
-    sectionVerMapa.style.display = "flex"
-    intervalo = setInterval(pintarPersonaje, 50)
 
-    // lienzo.fillRect(5,15,20,40)
 
     // Si el elemento con ID Hipoge tiene la propiedad CHECKED va a disparar una alerta
     if (inputHipodogue.checked) {
@@ -134,9 +147,10 @@ function seleccionarMacotaJugador () {
     } else{
         alert("Seleciona una mascota")
         reiniciarJuego()}
-
-    extraerAtaques(mascotaJugador) 
-
+        
+        extraerAtaques(mascotaJugador)         
+        sectionVerMapa.style.display = "flex"
+        iniciarMapa()
     seleccionarMacotaEnemigo()
     function extraerAtaques (mascotaJugador) {
         let ataques 
@@ -284,38 +298,118 @@ function aleatorio(min, max){
     return Math.floor( Math.random()* (max - min + 1)+min)
 }
 
-function pintarPersonaje () {
-    capipepo.x = capipepo.x + capipepo.velocidadX
-    capipepo.y = capipepo.y + capipepo.velocidadY
+function pintarCanvas () {
+    mascotaJugadorObjeto.x = mascotaJugadorObjeto.x + mascotaJugadorObjeto.velocidadX
+    mascotaJugadorObjeto.y = mascotaJugadorObjeto.y + mascotaJugadorObjeto.velocidadY
     lienzo.clearRect(0, 0, mapa.width, mapa.height)
     lienzo.drawImage(
-        capipepo.mapaFoto, 
-        capipepo.x,
-        capipepo.y,
-        capipepo.ancho,
-        capipepo.alto
-        )
+        mapaBackground,
+        0,
+        0,
+        mapa.width,
+        mapa.height
+    )
+    mascotaJugadorObjeto.pintarMokepon()
+    hipodogeEnemigo.pintarMokepon()
+    ratigueyaEnemigo.pintarMokepon()
+    capipepoEnemigo.pintarMokepon()
+    if (mascotaJugadorObjeto.velocidadX !== 0 || mascotaJugadorObjeto.velocidadY !== 0) {
+        revisarColision(hipodogeEnemigo)
+        revisarColision(capipepoEnemigo)
+        revisarColision(ratigueyaEnemigo)
+    }
     }
     
     
 function moverDerecha () {
-    capipepo.velocidadX = 5
+    mascotaJugadorObjeto.velocidadX = 5
 }
 function moverIzquierda () {
-    capipepo.velocidadX = - 5
+    mascotaJugadorObjeto.velocidadX = - 5
 
 }
 function moverArriba () {
-    capipepo.velocidadY = -5
+    mascotaJugadorObjeto.velocidadY = - 5
 
 }
 function moverAbajo () {
-    capipepo.velocidadY = 5
+    mascotaJugadorObjeto.velocidadY = 5
 }
 
 function detenerMovimiento () {
-    capipepo.velocidadX = 0
-    capipepo.velocidadY = 0
+    mascotaJugadorObjeto.velocidadX = 0
+    mascotaJugadorObjeto.velocidadY = 0
 }
 
+
+function sePresionaUnaTecla (event) {
+    switch (event.key) {
+        case "ArrowUp":
+            moverArriba()
+            break
+        case "ArrowDown":
+            moverAbajo()
+            break
+        case "ArrowLeft":
+            moverIzquierda()
+            break
+        case "ArrowRight":
+            moverDerecha()
+            break
+        default:
+            break;
+    }
+}
+
+
+function iniciarMapa() {
+    mapa.width = 320
+    mapa.height = 240
+    mascotaJugadorObjeto = ontenerObjetoMacota(mascotaJugador)
+    console.log(mascotaJugadorObjeto, mascotaJugador)
+    intervalo = setInterval(pintarCanvas, 50)
+    window.addEventListener("keydown", sePresionaUnaTecla)
+    window.addEventListener("keyup", detenerMovimiento)
+}
+
+
+
+function ontenerObjetoMacota() {
+    for (let i = 0; i < mokepones.length; i++) {
+        if (mascotaJugador === mokepones[i].nombre) {
+            return mokepones[i]
+        }
+    }
+
+    mostrarAtaques(ataques)
+}
+
+function revisarColision(enemigo) {
+    const arribaMascota = 
+    mascotaJugadorObjeto.y
+    const abajoMascota = 
+    mascotaJugadorObjeto.y + mascotaJugadorObjeto.alto
+    const derechaMascota = 
+    mascotaJugadorObjeto.y + mascotaJugadorObjeto.ancho
+    const izquierdaMascota = 
+    mascotaJugadorObjeto.x
+
+
+    const arribaEnemigo = enemigo.y
+    const abajoEnemigo = enemigo.y + enemigo.alto
+    const derechaEnemigo = enemigo.y + enemigo.ancho
+    const izquierdaEnemigo = enemigo.x
+    if (
+        abajoMascota < arribaEnemigo ||
+        arribaMascota > abajoEnemigo ||
+        derechaMascota < izquierdaEnemigo ||
+        izquierdaMascota > derechaEnemigo
+    ) {
+        return;
+    } detenerMovimiento() 
+    alert(`Haz colisionado con ${enemigo.nombre}`)
+}
+
+
 window.addEventListener("load", iniciarJuego)
+
